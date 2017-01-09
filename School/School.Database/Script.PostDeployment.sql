11a19b476a5a59668799c5418755707f63e36ac6
srisakthi2605@gmail.com <Mahalingam@123>
2017-01-09 06:07:40 -0800

    Adding files

---
+++ School/School.Database/Script.PostDeployment.sql
@@ -0,0 +1,23 @@
+﻿/*
+Post-Deployment Script Template							
+--------------------------------------------------------------------------------------
+ This file contains SQL statements that will be appended to the build script.		
+ Use SQLCMD syntax to include a file in the post-deployment script.			
+ Example:      :r .\myfile.sql								
+ Use SQLCMD syntax to reference a variable in the post-deployment script.		
+ Example:      :setvar TableName MyTable							
+               SELECT * FROM [$(TableName)]					
+--------------------------------------------------------------------------------------
+*/
+
+MERGE INTO Student AS Target
+USING (VALUES 
+        (1, 'Donnie', 15), 
+        (2, 'Liza', 15), 
+	    (3, 'Phil', 15)
+)
+AS Source (Id,Name, Age)
+ON Target.StudentID = Source.StudentID
+WHEN NOT MATCHED BY TARGET THEN
+INSERT (Name, Age)
+VALUES (Name, Age); 
---
+++ School/School.Database/Student.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Student]
+(
+	[Id] INT NOT NULL PRIMARY KEY,
+	[Name] NVARCHAR(200) NOT NULL,
+	[Age] INT NOT NULL
+)
